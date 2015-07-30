d2aafacb6acfd468d9a477f4e871c9ba9673a3bc
Paul Crane <paul@acheronsoft.com>
2015-07-30 01:26:49 -0500

    Initial commit

---
+++ source/package.d
@@ -0,0 +1,6 @@
+module raijin;
+
+public
+{
+	import raijin.commandlineargs;
+}
---
+++ source/raijin/commandlineargs.d
@@ -0,0 +1,108 @@
+module raijin.commandlineargs;
+
+import std.conv;
+import std.string : split, removechars;
+import std.traits : isNumeric;
+import std.stdio : writeln;
+
+public enum ProcessReturnValues { NOTPROCESSED, PROCESSED, INVALIDOPTION, NOARGS, HELP }
+
+class CommandLineArgs
+{
+public:
+	final T get(T = string)(immutable string key) @safe
+	{
+		if(isNumeric!T)
+		{
+			return to!T(values_.get(key, "0"));
+		}
+		else
+		{
+			return to!T(values_.get(key, ""));
+		}
+	}
+
+	final T get(T = string)(immutable string key, string defaultValue) @safe
+	{
+		return to!T(values_.get(key, defaultValue));
+	}
+
+	final string opIndex(immutable string key) @safe
+	{
+		return get(key);
+	}
+
+	final void opIndexAssign(string value, immutable string key) @safe
+	{
+		values_[key] = value;
+	}
+
+	final bool contains(immutable string key) @safe
+	{
+		if(key in values_)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	void printHelp() @trusted // NOTE: This should really be overriden since the default is very minimal.
+	{
+		writeln("The following options are available:");
+
+		foreach(key; values_.byKey)
+		{
+			writeln("  --", key);
+		}
+
+		writeln("  --help SHSHITITAFDDADSt");
+		writeln();
+	}
+
+	final ProcessReturnValues process(string[] arguments) @safe
+	{
+		auto elements = arguments[1 .. $]; // INFO: Remove program name.
+
+		if(elements.length > 0)
+		{
+			if(elements[0] == "--help")
+			{
+				// Call inherited help function
+				printHelp();
+				return ProcessReturnValues.HELP;
+			}
+
+			foreach(element; elements)
+			{
+				auto keyValue = element.split("=");
+
+				if(keyValue.length > 1)
+				{
+					auto key = keyValue[0].removechars("--");
+
+					if(contains(key))
+					{
+						values_[key] = keyValue[1];
+					}
+					else
+					{
+						return ProcessReturnValues.INVALIDOPTION;
+					}
+				}
+				else
+				{
+					return ProcessReturnValues.INVALIDOPTION;
+				}
+			}
+
+			return ProcessReturnValues.PROCESSED;
+		}
+		else
+		{
+			return ProcessReturnValues.NOARGS;
+		}
+	}
+
+private:
+	static string[string] values_;
+}
