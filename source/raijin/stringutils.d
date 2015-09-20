1dcdca72ca69fb7cc68389cb39516e01fba53be5
Paul Crane <paul@acheronsoft.com>
2015-09-20 01:18:54 -0500

    Add function pluralize used for pluralizing strings.

--- source/raijin/stringutils.d
+++ source/raijin/stringutils.d
@@ -7,6 +7,7 @@
 module raijin.stringutils;
 
 import std.string : indexOf;
+import std.conv : to;
 
 bool find(immutable string value, immutable string toFindValue) pure @safe
 {
@@ -17,3 +18,28 @@ bool find(immutable string value, immutable string toFindValue) pure @safe
 	return true;
 }
 
+string pluralize(string text, immutable uint count)
+{
+	string pluralizedNumber = text[0 .. $ - 1];
+
+	if(count == 1)
+	{
+		pluralizedNumber = "1 " ~ text;
+	}
+	else
+	{
+		dchar lastChar = text[$ - 1];
+
+		if(lastChar == 'y')
+		{
+			pluralizedNumber = text[0 .. $ - 1];
+			pluralizedNumber = to!string(count) ~" " ~ pluralizedNumber ~ "ies";
+		}
+		else
+		{
+			pluralizedNumber = to!string(count) ~" " ~ text ~ "s";
+		}
+	}
+
+	return pluralizedNumber;
+}
