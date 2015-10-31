2b67f99ca242dcab46ad2857dd4dcf3ffbd4ae5e
Paul Crane <paul@acheronsoft.com>
2015-10-31 00:47:35 -0500

    Add documentation for typeutils boolean functions.

--- source/raijin/typeutils.d
+++ source/raijin/typeutils.d
@@ -4,21 +4,58 @@ import std.typecons : Flag;
 
 alias AllowNumericBooleanValues = Flag!"allowNumericBooleanValues";
 
+/**
+*   Determines if a string is a boolean value using true and false as qualifiers.
+*
+*   Params:
+*       value = string to use.
+*
+*   Returns:
+*       true if the value is a boolean false otherwise.
+*/
+
 bool isStringABool(immutable string value) @trusted
 {
     return(value == "true" || value == "false");
 }
 
+/**
+*   Determines if a string is a boolean value using "1" and "0" as qualifiers.
+*
+*   Params:
+*       value = number string to use.
+*
+*   Returns:
+*       true if the value is a boolean false otherwise.
+*/
 bool isNumberABool(immutable string value) @trusted
 {
     return(value == "1" || value == "0");
 }
 
+/**
+*   Determines if a string is a boolean value using 1 and 0 as qualifiers.
+*
+*   Params:
+*       value = number to use.
+*
+*   Returns:
+*       true if the value is a boolean false otherwise.
+*/
 bool isNumberABool(immutable int value) @trusted
 {
     return(value == 1 || value == 0);
 }
 
+/**
+*   Determines if a string is a boolean value using isStringABool and isNumberABool as qualifiers.
+*
+*   Params:
+*       value = string to use.
+*
+*   Returns:
+*       true if the value is a boolean false otherwise.
+*/
 bool isBoolean(immutable string value)
 {
     return (isStringABool(value) || isNumberABool(value));
