e57aa4c8cbfc37b695f893fca26d0d017b9d068d
Paul Crane <paul@acheronsoft.com>
2015-11-03 01:31:04 -0600

    Add list function to typeutils module
    This is a cool function found in https://github.com/CyberShadow/ae/ library. I didn't want to import this entire library for this function so I've split it out of ae.

--- source/raijin/typeutils.d
+++ source/raijin/typeutils.d
@@ -64,3 +64,63 @@ bool isBoolean(immutable string value)
 {
     return (isStringABool(value) || isNumberABool(value));
 }
+
+/**
+ * Same as TypeTuple, but meant to be used with values.
+ *
+ * Example:
+ *   foreach (char channel; ValueTuple!('r', 'g', 'b'))
+ *   {
+ *     // the loop is unrolled at compile-time
+ *     // "channel" is a compile-time value, and can be used in string mixins
+ *   }
+ */
+
+ // NOTE: The following functions can be found in CyberShadows ae lib https://github.com/CyberShadow/ae/
+template ValueTuple(T...)
+{
+    alias T ValueTuple;
+}
+
+template RangeTupleImpl(size_t N, R...)
+{
+    static if (N==R.length)
+        alias R RangeTupleImpl;
+    else
+        alias RangeTupleImpl!(N, ValueTuple!(R, R.length)) RangeTupleImpl;
+}
+
+/// Generate a tuple containing integers from 0 to N-1.
+/// Useful for static loop unrolling. (staticIota)
+template RangeTuple(size_t N)
+{
+    alias RangeTupleImpl!(N, ValueTuple!()) RangeTuple;
+}
+
+/// Equivalent of PHP's `list` language construct:
+/// http://php.net/manual/en/function.list.php
+/// Works with arrays and tuples.
+/// Specify `null` as an argument to ignore that index
+/// (equivalent of `list(x, , y)` in PHP).
+auto list(Args...)(auto ref Args args)
+{
+import std.typecons;
+import std.format;
+
+struct List
+{
+    auto dummy() { return args[0]; }
+
+    void opAssign(T)(auto ref T t)
+    {
+        assert(t.length == args.length,
+            "Assigning %d elements to list with %d elements"
+            .format(t.length, args.length));
+
+        foreach (i; RangeTuple!(Args.length))
+            static if (!is(Args[i] == typeof(null)))
+                args[i] = t[i];
+    }
+    }
+    return List();
+}
