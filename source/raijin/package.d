7c1342ddaf2b0e1b9fcf8f6267ef3d77ae0ed6bb
Paul Crane <paul@acheronsoft.com>
2015-11-29 23:21:49 -0600

    Add pathutils module for dealing with system path based funtionality.

--- source/raijin/package.d
+++ source/raijin/package.d
@@ -5,6 +5,7 @@ public
 	import raijin.commandlineargs;
 	import raijin.configpath;
 	import raijin.keyvalueconfig;
+	import raijin.pathutils;
 	import raijin.stringutils;
 	import raijin.typeutils;
 }
