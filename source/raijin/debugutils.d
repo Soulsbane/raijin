99ca3d50ee2e45c9689e31b9615afaca101441bd
Paul Crane <paul@acheronsoft.com>
2015-12-21 01:45:44 -0600

    Add date and time output to debugLog function.

--- source/raijin/debugutils.d
+++ source/raijin/debugutils.d
@@ -7,6 +7,7 @@ module raijin.debugutils;
 import std.stdio;
 import std.algorithm;
 import std.path;
+import std.datetime;
 
 /**
 *   Outputs a message to stdout with function name, line number and file name.
@@ -17,5 +18,5 @@ import std.path;
 void debugLog(const string msg, const int lineNumber = __LINE__, const string fileName = __FILE__,
 	const string funcName = __FUNCTION__) @trusted
 {
-	debug writefln("%s[%s](%d): %s", fileName.baseName, funcName.findSplitAfter(".")[1], lineNumber, msg);
+	debug writefln("%s - %s[%s](%d): %s", Clock.currTime, fileName.baseName, funcName.findSplitAfter(".")[1], lineNumber, msg);
 }
