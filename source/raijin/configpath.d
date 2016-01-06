1ba2b5b16ee6142c0f494aff3b2c61af014302ea
Paul Crane <paul@acheronsoft.com>
2016-01-05 21:12:16 -0600

    Add documentation for configpath module.

--- source/raijin/configpath.d
+++ source/raijin/configpath.d
@@ -1,3 +1,6 @@
+/**
+*	Provides an easy way to work the users configuration directory.
+*/
 module raijin.configpath;
 
 import std.path : buildNormalizedPath, baseName, stripExtension;
@@ -5,14 +8,27 @@ import std.file : exists, mkdirRecurse, thisExePath;
 
 import standardpaths : writablePath, StandardPath;
 
+/**
+*	Allows for the creation and deletion of directories in the users configuration directory.
+*/
 class ConfigPath
 {
+	/**
+	*	Intializes the application name to executables name and setups up config directory.
+	*/
 	this()
 	{
 		applicationName_ = thisExePath.baseName.stripExtension;
 		configDirPath_ = writablePath(StandardPath.Config);
 	}
 
+	/**
+	*	Intializes the application name to executables name and setups up config directory using supplied arguments.
+	*
+	*	Params:
+	*		organizationName = Name of your organization.
+	*		applicationName = Name of your application.
+	*/
 	this(const string organizationName, const string applicationName)
 	{
 		organizationName_ = organizationName;
@@ -20,7 +36,13 @@ class ConfigPath
 		configDirPath_ = writablePath(StandardPath.Config);
 	}
 
-	string getConfigDir(string name = string.init)
+	/**
+	*	Retries the path to the users config directory with an optional path appended to the end.
+	*
+	*	Params:
+	*		name = Name of the directory to retrieve.
+	*/
+	string getConfigDir(const string name = string.init)
 	{
 		if(name == string.init)
 		{
@@ -30,6 +52,12 @@ class ConfigPath
 		return buildNormalizedPath(configDirPath_, organizationName_, applicationName_, name);
 	}
 
+	/**
+	*	Creates a directory in the users config directory.
+	*
+	*	Params:
+	*		name = Name of the directory to create.
+	*/
 	void createConfigDir(const string name = string.init)
 	{
 		string normalPath = buildNormalizedPath(getConfigDir(name));
@@ -40,7 +68,18 @@ class ConfigPath
 		}
 	}
 
-	private:
+	/**
+	*	Removes a directory from the users config directory.
+	*
+	*	Params:
+	*		name = Name of the directory to remove.
+	*/
+	void removeConfigDir(const string name)
+	{
+	}
+
+private:
+
 	string organizationName_;
 	string applicationName_;
 	string configDirPath_;
