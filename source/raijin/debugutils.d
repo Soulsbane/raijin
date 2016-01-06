9f509e6367e2ac24fceca22d84d4ac484344f6b2
Paul Crane <paul@acheronsoft.com>
2016-01-05 20:51:56 -0600

    Various debug logging fixups.

--- source/raijin/debugutils.d
+++ source/raijin/debugutils.d
@@ -9,36 +9,57 @@ import std.algorithm;
 import std.path;
 import std.datetime;
 
-/**
-*   Outputs a message to stdout with function name, line number and file name.
-*
-*   Params:
-*       msg = The message to be print to stdout.
-*		lineNumber = __LINE__
-*		fileName = __FILE__
-*		funcName = __FUNCTION__
-*/
-void debugLog(const string msg, const int lineNumber = __LINE__, const string fileName = __FILE__,
-	const string funcName = __FUNCTION__) @trusted
-{
-	debug writefln("%s - %s[%s](%d): %s", Clock.currTime, fileName.baseName, funcName.findSplitAfter(".")[1], lineNumber, msg);
-}
-
 struct Log
 {
-	static void info(const string msg, const int lineNumber = __LINE__, const string fileName = __FILE__,
-		const string funcName = __FUNCTION__) @trusted
+	/**
+	*   Outputs a message to stdout marked as type info with function name, line number, time and file name attached.
+	*
+	*   Params:
+	*		lineNumber = __LINE__
+	*		fileName = __FILE__
+	*		funcName = __FUNCTION__
+	*       msg = The message to be print to stdout.
+	*/
+	static void info(const string fileName = __FILE__, const string funcName = __FUNCTION__,
+		const int lineNumber = __LINE__, const string msg = "<None>") @trusted
 	{
-		debug writefln("[INFO] %s - %s[%s](%d): %s", Clock.currTime, fileName.baseName, funcName.findSplitAfter(".")[1], lineNumber, msg);
+		debug writefln("INFO [%s] - %s[%s](%d): %s", Clock.currTime, fileName.baseName, funcName.findSplitAfter(".")[1], lineNumber, msg);
 	}
-	static void warn(const string msg, const int lineNumber = __LINE__, const string fileName = __FILE__,
-		const string funcName = __FUNCTION__) @trusted
+
+	/**
+	*   Outputs a message to stdout marked as type warn with function name, line number, time and file name attached.
+	*
+	*   Params:
+	*		lineNumber = __LINE__
+	*		fileName = __FILE__
+	*		funcName = __FUNCTION__
+	*       msg = The message to be print to stdout.
+	*/
+	static void warn(const string fileName = __FILE__, const string funcName = __FUNCTION__,
+		const int lineNumber = __LINE__, const string msg = "<None>") @trusted
 	{
-		debug writefln("[WARN] %s - %s[%s](%d): %s", Clock.currTime, fileName.baseName, funcName.findSplitAfter(".")[1], lineNumber, msg);
+		debug writefln("WARN [%s] - %s[%s](%d): %s", Clock.currTime, fileName.baseName, funcName.findSplitAfter(".")[1], lineNumber, msg);
 	}
-	static void error(const string msg, const int lineNumber = __LINE__, const string fileName = __FILE__,
-		const string funcName = __FUNCTION__) @trusted
+
+	/**
+	*   Outputs a message to stdout marked as type error with function name, line number, time and file name attached.
+	*
+	*   Params:
+	*		lineNumber = __LINE__
+	*		fileName = __FILE__
+	*		funcName = __FUNCTION__
+	*       msg = The message to be print to stdout.
+	*/
+	static void error(const string fileName = __FILE__, const string funcName = __FUNCTION__,
+		const int lineNumber = __LINE__, const string msg = "<None>") @trusted
 	{
-		debug writefln("[ERROR] %s - %s[%s](%d): %s", Clock.currTime, fileName.baseName, funcName.findSplitAfter(".")[1], lineNumber, msg);
+		debug writefln("ERROR [%s] - %s[%s](%d): %s", Clock.currTime, fileName.baseName, funcName.findSplitAfter(".")[1], lineNumber, msg);
 	}
+}
+
+unittest
+{
+	Log.info("This is a info test");
+	Log.warn("This is a warn test");
+	Log.error("This is a error test");
 }
