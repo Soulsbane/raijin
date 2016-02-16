91707952a2eca31a84deab8cb15656ef2c764493
Paul Crane <paul@acheronsoft.com>
2016-02-16 01:03:45 -0600

    Add package. for utils modules.
    There is a bug at this point where the module debug can't be named debug which will be fixed in next commit.

--- source/raijin/package.d
+++ source/raijin/package.d
@@ -2,15 +2,10 @@ module raijin;
 
 public
 {
-	import raijin.utils.array;
+	import raijin.utils;
 	import raijin.appconfig;
 	import raijin.commandlineargs;
 	import raijin.cmdline;
 	import raijin.configpath;
-	import raijin.utils.debug;
-	import raijin.utils.file;
-	import raijin.utils.path;
-	import raijin.utils.string;
-	import raijin.utils.type;
 	import raijin.keyvalueconfig;
 }
---
+++ source/raijin/utils/package.d
@@ -0,0 +1,11 @@
+module raijin.utils;
+
+public
+{
+	import raijin.utils.array;
+	import raijin.utils.debuging; // DMD bug where module can't be named debug.
+	import raijin.utils.file;
+	import raijin.utils.path;
+	import raijin.utils.string;
+	import raijin.utils.type;
+}
