ccdaf7c7928334b9f80d081c20880abad6c7b378
Paul Crane <paul@acheronsoft.com>
2016-02-19 21:34:01 -0600

    Add example documentation for RepeatingTimer class.

--- source/raijin/timers.d
+++ source/raijin/timers.d
@@ -9,6 +9,25 @@ debug import std.stdio;
 
 alias dur = core.time.dur; // Avoids having to import core.time in the user's program.
 
+/**
+	A class for creating a RepeatingTimer.
+
+	Examples:
+		--------------------------------------
+		class MyRepeatingTimer : RepeatingTimer
+		{
+			override void onTimer()
+			{
+				writeln("MyRepeatingTimer");
+			}
+		}
+
+		auto delay = dur!("seconds")(1);
+
+		auto myTimer = new MyRepeatingTimer;
+		myTimer.start(delay);
+		--------------------------------------
+*/
 class RepeatingTimer
 {
 	void start(const Duration dur)
