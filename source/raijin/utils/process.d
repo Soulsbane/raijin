83c480737b325be651f7365efd913c81b4281727
Paul Crane <paul@acheronsoft.com>
2016-03-20 16:01:38 -0500

    Add documentation for launchApplication function.

--- source/raijin/utils/process.d
+++ source/raijin/utils/process.d
@@ -1,3 +1,9 @@
+/**
+	Small wrapper functions for std.process functions.
+
+	Authors:
+		Paul Crane
+*/
 module raijin.utils.process;
 
 import std.process;
@@ -7,6 +13,16 @@ import std.exception;
 
 import raijin.utils.path;
 
+/**
+	Small wrapper function that launches an application using std.process.executeShell.
+
+	Params:
+		fileName = Name of the application to launch.
+		args = The arguments to the program.
+
+	Returns:
+		Returns the same tuple as $(LINK2 http://dlang.org/phobos/std_process.html#executeShell, std.process.executeShell)
+*/
 auto launchApplication(const string fileName, const string[] args...)
 {
 	auto result = std.typecons.Tuple!(int, "status", string, "output")(127, "Executable not found.");
