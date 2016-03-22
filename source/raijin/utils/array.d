4c9b72f1bf30dccd30220199892ddcee377074f2
Paul Crane <paul@acheronsoft.com>
2016-03-22 01:10:03 -0500

    Add function insertValueInPlace function to utils.array module.

--- source/raijin/utils/array.d
+++ source/raijin/utils/array.d
@@ -40,3 +40,38 @@ unittest
 	test3.remove(2.2);
 	assert(test3 == [1.1, 3.3]);
 }
+
+/**
+	Inserts a value into an array after a given value in place.
+
+	Params:
+		array = The array to insert value into.
+		indexValue = The value to insert after.
+		insertAfterValue = The value to insert.
+*/
+void insertValueInPlace(T)(ref T[] array, T indexValue, T insertAfterValue)
+{
+	import std.array : insertInPlace;
+	size_t index = array.countUntil(indexValue);
+
+	if(index >= 0)
+	{
+		array.insertInPlace(++index, insertAfterValue);
+	}
+}
+
+unittest
+{
+	int[] test1 = [1, 2, 3, 4, 5, 6, 7];
+	string[] test2 = ["one", "two", "three"];
+	double[] test3 = [1.1, 2.2, 3.3];
+
+	test1.insertValueInPlace(5, 88);
+	assert(test1 == [1, 2, 3, 4, 5, 88, 6, 7]);
+
+	test2.insertValueInPlace("two", "fifteen");
+	assert(test2 == ["one", "two", "fifteen", "three"]);
+
+	test3.insertValueInPlace(3.3, 8.8);
+	assert(test3 == [1.1, 2.2, 3.3, 8.8]);
+}
