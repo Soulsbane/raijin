de7347761cf313aaf6ed8abea23d9b012fea0320
Paul Crane <paul@acheronsoft.com>
2016-03-21 19:57:23 -0500

    Add function overload for loadApplication.

--- source/raijin/utils/process.d
+++ source/raijin/utils/process.d
@@ -13,6 +13,9 @@ import std.exception;
 
 import raijin.utils.path;
 
+// BUG: DMD can't infer a return type for both launchApplication functions without this.
+alias LaunchApplicationReturnType = std.typecons.Tuple!(int, "status", string, "output");
+
 /**
 	Small wrapper function that launches an application using std.process.executeShell.
 
@@ -23,11 +26,26 @@ import raijin.utils.path;
 	Returns:
 		Returns the same tuple as $(LINK2 http://dlang.org/phobos/std_process.html#executeShell, std.process.executeShell)
 */
-auto launchApplication(const string fileName, const string[] args...) @safe
+LaunchApplicationReturnType  launchApplication(const string fileName, const string[] args...) @safe
+{
+	return launchApplication(fileName, args.join(' '));
+}
+
+/**
+	Small wrapper function that launches an application using std.process.executeShell.
+
+	Params:
+		fileName = Name of the application to launch.
+		args = The arguments to the program.
+
+	Returns:
+		Returns the same tuple as $(LINK2 http://dlang.org/phobos/std_process.html#executeShell, std.process.executeShell)
+*/
+LaunchApplicationReturnType launchApplication(const string fileName, const string args) @safe
 {
 	auto result = std.typecons.Tuple!(int, "status", string, "output")(127, "Executable not found.");
 	immutable auto inPath = isInPath(fileName);
-	immutable string fileNameAndArgs = fileName ~ ' ' ~ args.join(' ');
+	immutable string fileNameAndArgs = fileName ~ ' ' ~ args;
 
 	if(fileName.exists)
 	{
@@ -40,5 +58,15 @@ auto launchApplication(const string fileName, const string[] args...) @safe
 	else
 	{
 		return result;
+	}
+}
+
+///
+unittest
+{
+	version(Linux)
+	{
+		auto result = launchApplication("ls", "-l");
+		assert(result.status == 0);
 	}
 }
