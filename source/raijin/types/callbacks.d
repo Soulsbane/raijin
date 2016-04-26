23b475ce26951f6a09080a803af1437861d6af14
Paul Crane <paul@acheronsoft.com>
2016-04-25 22:06:03 -0500

    Add documentation for callbacks module.

--- source/raijin/types/callbacks.d
+++ source/raijin/types/callbacks.d
@@ -1,5 +1,30 @@
+/**
+	Basic interface for working with callbacks(wrapped functions/delegates).
+
+	Authors:
+		Paul Crane
+*/
 module raijin.types.callbacks;
 
+/*
+	A simple interface for holding either a function or delegate to be called later.
+
+	Examples:
+		alias VoidDelegate = void delegate();
+
+		void func()
+		{
+			import std.stdio : writeln;
+			writeln("Called func..");
+		}
+
+		void main()
+		{
+			Callback!VoidDelegate func;
+			func.set(&func);
+			func();
+		}
+*/
 struct Callback(T)
 {
 	void opCall(Args...)(Args args)
@@ -10,11 +35,22 @@ struct Callback(T)
 		}
 	}
 
+	/**
+		Sets the callback used with opCall
+
+		callback = The callback function/delegate to use with opCall.
+	*/
 	void set(T callback)
 	{
 		callback_ = callback;
 	}
 
+	/**
+		Returns whether the callback has a valid function/delegate assigned to it.
+
+		Returns:
+			Whether the callback has a valid function/delegate assigned to it
+	*/
 	bool isSet() const
 	{
 		if(callback_)
