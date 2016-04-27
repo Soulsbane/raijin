e6f725d618e5cadedd0b5bfc6068664ddad4eb75
Paul Crane <paul@acheronsoft.com>
2016-04-27 00:56:06 -0500

    Add documentation for SimpleQueue type.

--- source/raijin/types/queue.d
+++ source/raijin/types/queue.d
@@ -1,14 +1,35 @@
+/**
+	Simple module for dealing with queues.
+
+	Authors:
+		Paul Crane
+*/
 module raijin.types.queue;
 
 import std.container : DList;
 
+/**
+	A simple implementation of a queue.
+*/
 struct SimpleQueue(T)
 {
+	/**
+		Pushes a value into the queue.
+
+		Params:
+			value = The value to push onto the queue.
+	*/
 	void push(const T value) pure @safe
 	{
 		data_.insertFront(value);
 	}
 
+	/**
+		Pops the value at the queues back off the queue.
+
+		Returns:
+			The value that was popped.
+	*/
 	T pop() pure @safe
 	{
 		T value;
@@ -22,11 +43,23 @@ struct SimpleQueue(T)
 		return value;
 	}
 
+	/**
+		Retrieves the value at the back of the queue(the first value pushed).
+
+		Returns:
+			The value at the back of the queue.
+	*/
 	T back() pure const @safe
 	{
 		return data_.back;
 	}
 
+	/**
+		Determines if the queue is empty.
+
+		Returns:
+			true if the queue is empty false otherwise.
+	*/
 	bool empty() pure const @safe
 	{
 		return data_.empty;
