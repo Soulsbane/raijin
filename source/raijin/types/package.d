512cdab8d73c89ed3c52c5644c1cfca34091649a
Paul Crane <paul@acheronsoft.com>
2016-04-27 01:29:01 -0500

    Add simple stack data type.

--- source/raijin/types/package.d
+++ source/raijin/types/package.d
@@ -2,10 +2,11 @@ module raijin.types;
 
 public
 {
-	import raijin.types.queue;
 	import raijin.types.boolean;
 	import raijin.types.callbacks;
 	import raijin.types.dynamic;
 	import raijin.types.functions;
+	import raijin.types.queue;
 	import raijin.types.records;
+	import raijin.types.stack;
 }
---
+++ source/raijin/types/stack.d
@@ -0,0 +1,82 @@
+/**
+	Simple module for dealing with stacks.
+
+	Authors:
+		Paul Crane
+*/
+module raijin.types.stack;
+
+import std.container : DList;
+
+/**
+	A simple implementation of a stack.
+*/
+struct SimpleStack(T)
+{
+	/**
+		Pushes a value onto the stack.
+
+		Params:
+			value = The value to push onto the stack.
+	*/
+	void push(const T value) pure @safe
+	{
+		data_.insertBack(value);
+	}
+
+	/**
+		Pops the value off the stack.
+
+		Returns:
+			The value that was popped.
+	*/
+	T pop() pure @safe
+	{
+		T value;
+
+		if(!data_.empty)
+		{
+		 	value = data_.back;
+			data_.removeBack;
+		}
+
+		return value;
+	}
+
+	/**
+		Retrieves the value at the back of the stack(the last value pushed) without popping.
+
+		Returns:
+			The value at the back of the stack.
+	*/
+	T back() pure const @safe
+	{
+		return data_.back;
+	}
+
+	/**
+		Determines if the stack is empty.
+
+		Returns:
+			true if the stack is empty false otherwise.
+	*/
+	bool empty() pure const @safe
+	{
+		return data_.empty;
+	}
+private:
+	DList!T data_;
+}
+
+///
+unittest
+{
+	SimpleStack!int stack;
+
+	stack.push(1);
+	stack.push(2);
+	stack.push(3);
+	assert(stack.pop() == 3);
+	assert(!stack.empty);
+	assert(stack.back == 2);
+}
