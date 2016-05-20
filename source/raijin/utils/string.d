3f0683745a9ae327bc816fd8c71086cfd9cd8755
Paul Crane <paul@acheronsoft.com>
2016-05-19 19:52:50 -0500

    Add function formatNumber to util.string module.

--- source/raijin/utils/string.d
+++ source/raijin/utils/string.d
@@ -412,3 +412,51 @@ unittest
 	assert("true".toBoolean == true);
 	assert("false".toBoolean == false);
 }
+
+/**
+	Formats a number using commas. Example 1000 => 1,000.
+
+	Params:
+		number = The number to format.
+
+	Returns:
+		The formatted number;
+*/
+string formatNumber(const string number)
+{
+	import std.regex : regex, replaceAll;
+
+	auto re = regex(r"(?<=\d)(?=(\d\d\d)+\b)","g");
+	return number.replaceAll(re, ",");
+}
+
+///
+unittest
+{
+	assert(formatNumber("100") == "100");
+	assert(formatNumber("1000") == "1,000");
+	assert(formatNumber("1000000") == "1,000,000");
+}
+
+/**
+	Formats a number using commas. Example 1000 => 1,000.
+
+	Params:
+		number = The number to format.
+
+	Returns:
+		The formatted number;
+*/
+string formatNumber(const size_t number)
+{
+	import std.conv : to;
+	return number.to!string.formatNumber;
+}
+
+///
+unittest
+{
+	assert(formatNumber(100) == "100");
+	assert(formatNumber(1000) == "1,000");
+	assert(formatNumber(1000000) == "1,000,000");
+}
