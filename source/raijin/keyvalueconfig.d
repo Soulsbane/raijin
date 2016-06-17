9a8dd9f975da48e07ed25a9cba7dbdac731317b7
Paul Crane <paul@acheronsoft.com>
2016-06-16 19:14:55 -0500

    Fix wording for save(string).

--- source/raijin/keyvalueconfig.d
+++ source/raijin/keyvalueconfig.d
@@ -274,7 +274,7 @@ public
 		Saves config values to the config file.
 
 		Params:
-			fileName = Name of the file which values will be stored. "app.config" by default.
+			fileName = Name of the file which values will be stored. 
 	*/
 	void save(string fileName) @trusted
 	{
