67ea0b804f45d2f74b632773a7afc32be53f4fb7
Paul Crane <paul@acheronsoft.com>
2016-12-09 02:53:19 -0600

    Mark initDefaultValue as @safe.

--- source/raijin/utils/safeindexargs.d
+++ source/raijin/utils/safeindexargs.d
@@ -20,7 +20,7 @@ import raijin.types.functions;
 	Returns:
 		The converted value.
 */
-T initDefaultValue(T = string)(const T defaultValue = T.init)
+T initDefaultValue(T = string)(const T defaultValue = T.init) @safe
 {
 
 	T value = defaultValue;
