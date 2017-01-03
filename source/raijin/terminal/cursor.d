8421347274ee901f30f26996da43b230b7eecffc
Paul Crane <paul@acheronsoft.com>
2017-01-03 00:31:08 -0600

    Add documentation for cursor module.

--- source/raijin/terminal/cursor.d
+++ source/raijin/terminal/cursor.d
@@ -1,3 +1,6 @@
+/**
+	Provides functions for showing/hiding the mouse cursor in a terminal.
+*/
 module raijin.terminal.cursor;
 
 import std.stdio : write;
@@ -5,12 +8,17 @@ import std.stdio : write;
 private immutable SHOW_CURSOR = "\x1b[?25h";
 private immutable HIDE_CURSOR = "\x1b[?25l";
 
-
+/**
+	Shows the cursor.
+*/
 void showCursor()
 {
 	write(SHOW_CURSOR);
 }
 
+/**
+	Hides the cursor.
+*/
 void hideCursor()
 {
 	write(HIDE_CURSOR);
