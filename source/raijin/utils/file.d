1746af95e1d7e5c3a2356ba3762dfd429563ba12
Paul Crane <paul@acheronsoft.com>
2017-01-05 22:47:35 -0600

    Remove extractor module code and import dfileutils.extractor code instead.

--- source/raijin/utils/file.d
+++ source/raijin/utils/file.d
@@ -17,6 +17,7 @@ import std.array;
 import std.typetuple;
 import std.typecons;
 
+public import dfileutils.extractor;
 import raijin.utils;
 alias OverwriteExtractedFiles = Flag!"OverwriteExtractedFiles";
 
@@ -138,61 +139,3 @@ unittest
 	assert(nonHiddenFile.removeFileIfExists);
 }
 
-private string getFilesList(T)(T list)
-{
-	return "TypeTuple!(" ~ list.map!(a => `"` ~ a ~ `"`).join(",") ~ ")";
-}
-
-private template GeneratorFileNames(string[] list)
-{
-	mixin("private alias GeneratorFileNames = " ~ getFilesList(list)~ ";");
-}
-
-/**
-	Uses a list of files that will be imported using D's string import functionality. The string import for each files
-	will then be written according to the path parameter and the name of the file in the file list.
-
-	Note that you will need to set the string import path useing the -J switch if using DMD or the stringImportPaths
-	configuration variable if using DUB.
-
-	Params:
-		list = Must be an enum string list of filenames that will be imported and written later using the same name.
-		path = The path where the files should be exported to.
-		overwrite = Whether to overwrite existing files.
-
-	Examples:
-		enum filesList =
-		[
-			"resty/template.lua",
-			"helpers.lua"
-		];
-
-		// Each file will be will be created in this format: ./myawesomeapp/resty/template.lua
-		extractImportFiles!filesList("myawesomeapp");
-*/
-void extractImportFiles(alias list, T = string)(const string path, OverwriteExtractedFiles overwrite = OverwriteExtractedFiles.yes)
-{
-	foreach(name; GeneratorFileNames!(list))
-	{
-		immutable string filePath = dirName(buildNormalizedPath(path, name));
-		immutable string pathWithFileName = buildNormalizedPath(path, name);
-		T content;
-
-		static if(is(T : string))
-		{
-			content = import(name);
-		}
-		else
-		{
-			content = cast(T)import(name);
-		}
-
-		if(overwrite)
-		{
-			removeFileIfExists(pathWithFileName);
-		}
-
-		ensurePathExists(filePath);
-		ensureFileExists(pathWithFileName, content);
-	}
-}
